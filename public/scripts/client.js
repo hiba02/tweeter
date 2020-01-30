eccbe8088c785398786e8fc8c5a7f3a2077237ae
yoon soon kang <kangyoonsoon@hotmail.com>
2020-01-30 14:40:09 +0000

    index.html: remove header to adjust height |  client.js: ajax POST for data sumbission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,62 +10,29 @@
 ////////////////////////////////////////////////////////////////////////
 //Remove the hard-coded tweets object that we added earlier in order to drive our renderTweets function.
 ////////////////////////////////////////////////////////////////////////
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-
-
 
 
 
 
 $(document).ready(function() {
-  // $tweet = createTweetElement();
-  // $('#tweet-container').append($tweet);
-
-  //Ajax -- tweet button 
-  // $(function() {
-    
-  //   $button.on('click', function () {
-  //     event.preventDefault();
-  //     $.ajax("test.html", { method: 'POST' })
-  //     .then(function (morePostsHtml) {
-  //       $button.replaceWith(morePostsHtml);
-  //     });
-  //   });
-  // });
-
-
-
 
   const testComments = function() {
     // $('#testAjax').text('Hello world');
-    $('#testAjax').text($('form').serialize());
+    //$('#testAjax').text($('form').serialize());
+    event.preventDefault();
     const tweetComments = $('form').serialize();
     console.log(tweetComments);
     console.log(tweetComments.length);
-
   };
 
-
-
-  ///??? POST => 404 error!!
-  // need to change GET to POST
+  ///?????????????
+  // event.preventDefault(); not working!!
   const $button = $('.tweetButton');
-  $button.on('click', function() {
+  $button.submit(function(event) {
     event.preventDefault();
-    $.ajax('http://localhost:8080/tweets',{
-        method: 'GET',
-        name: name 
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:8080/tweets'
     })
     .then(testComments)
   });
@@ -92,10 +59,7 @@ $(document).ready(function() {
 
 
 const createTweetElement = function(tweet) {
-  // let $tweet = $('<article>').addClass('tweet');
-  // // ...
-  // return $tweet;
-    
+
     let $tweet = `<article class="tweet-container">
     <div class="tweet-container-header">
       <div style="flex-grow: 1;">
@@ -116,32 +80,25 @@ const createTweetElement = function(tweet) {
       <span>${tweet.created_at}</span>
       <span class="counter">icons</span>
     </footer>
-  
   </article>`
   return $tweet;
-  }
+};
 
 
 
   const renderTweets = function(tweetsArray) {
-
-// loops through tweets
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
-
     console.log(tweetsArray);
     $tweet = '';
     for (let eachTweet of tweetsArray) {
-      console.log('eachTweet.user.name:  ', eachTweet.user.name);
-      console.log('eachTweet.user.avatars:  ', eachTweet.user.avatars);      
-      console.log('eachTweet.user.handle:  ', eachTweet.user.handle);
-      console.log('eachTweet.content.text:  ', eachTweet.content.text);
-      console.log('eachTweet.created_at: ', eachTweet.created_at);
+      // console.log('eachTweet.user.name:  ', eachTweet.user.name);
+      // console.log('eachTweet.user.avatars:  ', eachTweet.user.avatars);      
+      // console.log('eachTweet.user.handle:  ', eachTweet.user.handle);
+      // console.log('eachTweet.content.text:  ', eachTweet.content.text);
+      // console.log('eachTweet.created_at: ', eachTweet.created_at);
       $tweet += createTweetElement(eachTweet);
     }
     $('#tweet-container').append($tweet);
   };
-
 
 
   const loadTweets = function() {
@@ -151,27 +108,8 @@ const createTweetElement = function(tweet) {
     })
     .then(renderTweets);
   };
-
   loadTweets();
 
-
-
-
-
-
-
-
-
-//   $('.tweet-container')
-//   .mouseover(function(){
-//     $(this).removeClass('tweet-container')
-//     .addClass('tweet-container-shadow');
-//     console.log('mouse in');
-//   })
-//   .mouseout(function(){
-//     $(this).removeClass('tweet-container-shadow')
-//     .addClass('tweet-container');
-//   })
 
 });
 
